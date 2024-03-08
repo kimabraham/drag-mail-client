33ed99f204c9ab60dee0f33bb8630d069a198442
kimabraham <kimkr88@icloud.com>
2024-03-09 00:39:34 +0900

    Feat:home screen

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,11 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import { styled } from "styled-components";
+
+const MainContainer = styled.div`
+    max-width: 1280px;
+    margin: auto;
+`;
 
 const Layout = () => {
   return (
     <div>
       <Header />
-      <Outlet />
+      <MainContainer>
+        <Outlet />
+      </MainContainer>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,93 @@
+import { Link } from "react-router-dom";
+import { styled } from "styled-components";
+import { TbTemplate } from "react-icons/tb";
+import homeImage from "../assets/imgs/home_image.png";
+
+const Container = styled.div`
+    display: flex;
+    max-width: 1080px;
+    margin:auto;
+    & > :first-child{
+      width: 45%;
+    }
+    & > :last-child{
+      width: 55%;
+    }
+  `;
+
+const Box = styled.div`
+    padding-top: 90px;
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+  `;
+
+const Title = styled.h2`
+    margin-bottom: 20px;
+  `;
+
+const Desc = styled.p`
+    line-height: 40px;
+    word-spacing: 2px;
+    font-size: 1.1rem;
+    word-wrap: break-word;
+  `;
+
+const Button = styled.button`
+    display: inline-flex;
+    gap: 4px;
+    justify-content: center;
+    align-items: center;
+    width: 40%;
+    color: white;
+    background-color: ${(props) => props.theme.accent};
+    margin-left: 40px;
+    padding: 10px;
+    border: none;
+    border-radius: 10px;
+    font-size: larger;
+    letter-spacing: .2px;
+    cursor: pointer;
+  `;
+
+const HomeImage = styled.img.attrs((props) => ({
+  src: homeImage,
+}))`
+    margin-top: 60px;
+    width:100%;
+  `;
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Box>
+        <div>
+          <Title>Drag N Drop</Title>
+          <Title>E-mail template</Title>
+          <Title>builder</Title>
+        </div>
+        <Desc>
+          드래그 앤 드롭으로 당신의 아이디어를 현실로! <br />
+          코딩 없이도 멋진 이메일 템플릿을 만들 수 있는 <br />
+          간편한 방법을 경험하세요.
+          <br />
+          우리의 사용자 친화적인 인터페이스로
+          <br />
+          마케팅 이메일을 다음 단계로 끌어올리세요.
+          <br /> 지금 바로 우리의 드래그 앤 드롭 툴을 체험해 보세요!
+        </Desc>
+        <Link to="/demo">
+          <Button>
+            Try Demo
+            <TbTemplate />
+          </Button>
+        </Link>
+      </Box>
+      <Box>
+        <HomeImage />
+      </Box>
+    </Container>
+  );
 };
 
 export default Home;
