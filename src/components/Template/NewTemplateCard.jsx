c369ff4408b05913c7bbc8b4d489b28c726ea0d2
kimabraham <kimkr88@icloud.com>
2024-03-11 02:59:39 +0900

    Feat: Create NewTemplateCard

---
+++ src/components/Template/NewTemplateCard.jsx
@@ -0,0 +1,55 @@
+import axios from "axios";
+import { styled } from "styled-components";
+import { FaPlus } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
+const Template = styled.div`
+  width: 180px;
+  height: 250px;
+  border: 2px solid #555c67;
+  margin-bottom: 40px;
+  cursor: pointer;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #747d8c;
+  & > h5, & > svg {
+    margin-top: 20px;
+    color: white;
+  }
+  &:hover{
+    background-color: white;
+    & > h5, & > svg {
+      color: #747d8c;
+    }
+  }
+`;
+
+const NewTemplateCard = () => {
+  const navigate = useNavigate();
+
+  const handleCreate = async () => {
+    const res = await axios.post(
+      "/api/projects",
+      {},
+      { withCredentials: true }
+    );
+    const {
+      data: {
+        project: { _id },
+      },
+    } = res;
+    navigate(`/template/${_id}`);
+  };
+
+  return (
+    <Template onClick={handleCreate}>
+      <FaPlus size={40} />
+      <h5>New template</h5>
+    </Template>
+  );
+};
+
+export default NewTemplateCard;
