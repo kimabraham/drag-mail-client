d9da95647f051075854642a0151d7c00ff248664
kimabraham <kimkr88@icloud.com>
2024-03-11 03:00:20 +0900

    Feat: Create My template page

--- src/pages/MyTemplates.jsx
+++ src/pages/MyTemplates.jsx
@@ -1,5 +1,55 @@
+import axios from "axios";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { styled } from "styled-components";
+
+import NewTemplateCard from "../components/Template/NewTemplateCard";
+import TemplateCard from "../components/Template/TemplateCard";
+
+const Container = styled.div`
+  width: 80%;
+  padding: 150px 0px;
+  margin: auto;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(180px, 1fr));
+  column-gap: 40px;
+`;
+
 const MyTemplates = () => {
-  return <div>MyTemplates</div>;
+  const queryClient = useQueryClient();
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["get-user-projects"],
+    queryFn: async () => {
+      const res = await axios.get(`/api/users/projects`, {
+        withCredentials: true,
+      });
+      return res.data.projects;
+    },
+  });
+
+  const deleteMutation = useMutation({
+    mutationFn: (projectId) =>
+      axios.delete(`/api/projects/${projectId}`, { withCredentials: true }),
+    onSuccess: (data, variables) => {
+      const projectId = variables;
+      queryClient.setQueryData(["get-user-projects"], (oldProjects) => {
+        return oldProjects.filter((project) => project._id !== projectId);
+      });
+    },
+  });
+
+  return (
+    <Container>
+      <NewTemplateCard />
+      {data?.map((template) => (
+        <TemplateCard
+          key={template._id}
+          template={template}
+          deleteProject={() => deleteMutation.mutate(template._id)}
+        />
+      ))}
+    </Container>
+  );
 };
 
 export default MyTemplates;
