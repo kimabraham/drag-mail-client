34ee1202e43165405f9c8ca1115caab3e7726177
kimabraham <kimkr88@icloud.com>
2024-03-12 23:50:09 +0900

    Feat: Add block cards

--- src/pages/CreateTemplate.jsx
+++ src/pages/CreateTemplate.jsx
@@ -1,8 +1,14 @@
+import React, { useEffect, useRef, useState } from "react";
 import Logo from "../components/shared/Logo";
 import { styled } from "styled-components";
 import { FaCaretDown } from "react-icons/fa";
 import Profile from "../components/Profile";
-import React, { useEffect, useRef, useState } from "react";
+import { PiTextT, PiImage, PiVideoCamera, PiTimerBold } from "react-icons/pi";
+import { FiInstagram } from "react-icons/fi";
+import { RxButton } from "react-icons/rx";
+import { LuAlignVerticalSpaceAround } from "react-icons/lu";
+import { TfiLayoutMenuSeparated } from "react-icons/tfi";
+
 import ContainerCard from "../components/Email/ContainerCard";
 import TemplateTitle from "../components/Email/TemplateTitle";
 import useAuthStatus from "../hooks/useAuthStatus";
@@ -75,20 +81,6 @@ const Structures = styled.div`
     }
 `;
 
-const Blocks = styled.div`
-    display: flex;
-    flex-direction: column;
-    & > :first-child {
-        color: #57606f;
-        background-color:#ced6e0;
-        display: flex;
-        align-items: center;
-        gap:10px;
-        padding: 5px 15px;
-        border-bottom: 1px solid;
-    }
-`;
-
 const StructureList = styled.ul`
   display: flex;
   flex-direction: column;
@@ -119,8 +111,61 @@ const StructureItem = styled.li`
   }
 `;
 
-const Row = styled.div``;
-const Card = styled.div``;
+const Blocks = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    & > :first-child {
+        color: #57606f;
+        background-color:#ced6e0;
+        display: flex;
+        align-items: center;
+        gap:10px;
+        padding: 5px 15px;
+        border-bottom: 1px solid;
+    }
+    & > :last-child {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
+      background-color: #ecf0f1;
+      padding: 30px;
+    }
+`;
+
+const Row = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  gap: 20px;
+`;
+
+const Card = styled.div`
+  width: 100%;
+  border: 1px solid #747d8c;
+  border-radius: 10px;
+  cursor: pointer;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #70a1ff;
+  gap:10px;
+  & > span {
+    color: #747d8c;
+    font-size: 15px;
+    font-weight: lighter;
+    letter-spacing: .5px;
+  }
+  font-size: larger;
+  &:hover {
+    border: 1px solid #3742fa;
+    box-shadow: 0 0 0 2px #3742fa;
+  }
+`;
 
 const CreateTemplate = () => {
   useAuthStatus();
@@ -135,7 +180,9 @@ const CreateTemplate = () => {
     }
   }, [components]);
 
-  const handleDragStart = (e) => {
+  console.log(components);
+
+  const handleContainerDragStart = (e) => {
     const target = e.target;
     const id = e.target.childNodes.length;
     e.dataTransfer.setData("text/plain", id);
@@ -152,6 +199,24 @@ const CreateTemplate = () => {
     setTimeout(() => document.body.removeChild(dragImage), 0);
   };
 
+  const handleBlockDragStart = (e) => {
+    const target = e.target;
+    const id = e.target.childNodes.length;
+
+    e.dataTransfer.setData("text/plain", id);
+
+    const dragImage = target.cloneNode(true);
+
+    dragImage.style.width = "100px";
+    dragImage.style.height = "100px";
+    dragImage.style.border = "2px solid #3742fa";
+    dragImage.style.borderRadius = "15px";
+    document.body.appendChild(dragImage);
+
+    e.dataTransfer.setDragImage(dragImage, target.offsetWidth / 2, 0);
+    setTimeout(() => document.body.removeChild(dragImage), 0);
+  };
+
   const createContainerCards = (id) => {
     return Array.from({ length: id }, (_, index) => (
       <ContainerCard key={index} />
@@ -160,6 +225,7 @@ const CreateTemplate = () => {
 
   const handleDrop = (e) => {
     e.preventDefault();
+
     const id = e.dataTransfer.getData("text/plain");
     const newComponents = createContainerCards(id);
     setComponents((prev) => [
@@ -171,6 +237,8 @@ const CreateTemplate = () => {
   const handleDragOver = (e) => {
     e.preventDefault();
   };
+
+  const handleBlockDragEnd = (e) => {};
 
   return (
     <Container>
@@ -196,20 +264,20 @@ const CreateTemplate = () => {
             <StructureList>
               <StructureItem
                 draggable="true"
-                onDragStart={(e) => handleDragStart(e)}
+                onDragStart={(e) => handleContainerDragStart(e)}
               >
                 <div></div>
               </StructureItem>
               <StructureItem
                 draggable="true"
-                onDragStart={(e) => handleDragStart(e)}
+                onDragStart={(e) => handleContainerDragStart(e)}
               >
                 <div></div>
                 <div></div>
               </StructureItem>
               <StructureItem
                 draggable="true"
-                onDragStart={(e) => handleDragStart(e)}
+                onDragStart={(e) => handleContainerDragStart(e)}
               >
                 <div></div>
                 <div></div>
@@ -217,7 +285,7 @@ const CreateTemplate = () => {
               </StructureItem>
               <StructureItem
                 draggable="true"
-                onDragStart={(e) => handleDragStart(e)}
+                onDragStart={(e) => handleContainerDragStart(e)}
               >
                 <div></div>
                 <div></div>
@@ -228,21 +296,70 @@ const CreateTemplate = () => {
           </Structures>
           <Blocks>
             <div>
-              <FaCaretDown size={30} />
+              <PiTextT />
               <h5>Blocks</h5>
             </div>
             <div>
               <Row>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
+                <Card
+                  draggable="true"
+                  onDragStart={handleBlockDragStart}
+                  onDragEnd={handleBlockDragEnd}
+                >
+                  <PiTextT size={30} />
+                  <span>Text</span>
+                </Card>
+                <Card
+                  draggable="true"
+                  onDragStart={(e) => handleBlockDragStart(e)}
+                >
+                  <PiImage size={30} />
+                  <span>Image</span>
+                </Card>
+                <Card
+                  draggable="true"
+                  onDragStart={(e) => handleBlockDragStart(e)}
+                >
+                  <RxButton size={30} />
+                  <span>Button</span>
+                </Card>
+                <Card
+                  draggable="true"
+                  onDragStart={(e) => handleBlockDragStart(e)}
+                >
+                  <LuAlignVerticalSpaceAround size={30} />
+                  <span>Space</span>
+                </Card>
               </Row>
               <Row>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
+                <Card
+                  draggable="true"
+                  onDragStart={(e) => handleBlockDragStart(e)}
+                >
+                  <PiVideoCamera size={30} />
+                  <span>Video</span>
+                </Card>
+                <Card
+                  draggable="true"
+                  onDragStart={(e) => handleBlockDragStart(e)}
+                >
+                  <FiInstagram size={30} />
+                  <span>Social</span>
+                </Card>
+                <Card
+                  draggable="true"
+                  onDragStart={(e) => handleBlockDragStart(e)}
+                >
+                  <TfiLayoutMenuSeparated size={30} />
+                  <span>Menu</span>
+                </Card>
+                <Card
+                  draggable="true"
+                  onDragStart={(e) => handleBlockDragStart(e)}
+                >
+                  <PiTimerBold size={30} />
+                  <span>Timer</span>
+                </Card>
               </Row>
             </div>
           </Blocks>
