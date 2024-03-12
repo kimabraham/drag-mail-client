450d96722069bf3dadb21ffadbd3bab181828ffc
kimabraham <kimkr88@icloud.com>
2024-03-12 11:34:17 +0900

    Feat: Create send mail page

--- src/pages/SendMail.jsx
+++ src/pages/SendMail.jsx
@@ -1,5 +1,23 @@
+import { styled } from "styled-components";
+
+const Container = styled.div`
+  width: 80%;
+  padding: 100px 0px;
+  margin: auto;
+  & > div {
+    display: flex;
+  }
+`;
+
 const SendMail = () => {
-  return <div>SendMail</div>;
+  return (
+    <Container>
+      <div>
+        <h3>Send mail</h3>
+        <button>Send</button>
+      </div>
+    </Container>
+  );
 };
 
 export default SendMail;
