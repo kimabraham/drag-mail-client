32f87e20b602b6586e74db6916baed6ced2b4ba4
kimabraham <kimkr88@icloud.com>
2024-03-12 14:58:33 +0900

    Feat: Create container layout using drag and drop

--- src/pages/CreateTemplate.jsx
+++ src/pages/CreateTemplate.jsx
@@ -2,9 +2,11 @@ import Logo from "../components/shared/Logo";
 import { styled } from "styled-components";
 import { FaCaretDown } from "react-icons/fa";
 import Profile from "../components/Profile";
+import React, { useState } from "react";
 
 const Container = styled.div`
     margin: auto;
+
 `;
 
 const Header = styled.header`
@@ -29,6 +31,8 @@ const Side = styled.div`
     display: flex;
     flex-direction: column;
     border-left: 1px solid;
+    overflow-y: auto;
+    right: 0;
     & > div{
         height: 50%;
     }
@@ -40,6 +44,30 @@ const Side = styled.div`
 const Screen = styled.div`
     width: 70%;
     background-color: #f1f2f6;
+    overflow-y: auto;
+`;
+
+const Content = styled.div`
+      width: 600px;
+      margin: 100px auto;
+      min-height: 100px;
+      background-color: white;
+      border: 1px solid;
+      & > div {
+        height: 100px;
+        padding: 10px 10px 5px 10px;
+        display: flex;
+        gap: 10px;
+        & > div {
+          border: 1px dashed #1e90ff;
+          border-radius: 5px;
+          width: 100%;
+          height:100%;
+        }
+      }
+      & > div:last-child {
+        padding-bottom: 10px;
+      }
 `;
 
 const Structures = styled.div`
@@ -70,10 +98,88 @@ const Blocks = styled.div`
     }
 `;
 
+const StructureList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  background-color: #ecf0f1;
+  padding: 20px 30px;
+  gap: 20px;
+`;
+
+const StructureItem = styled.li`
+  display: flex;
+  gap: 10px;
+  box-sizing: border-box;
+  height: 100%;
+  padding: 15px;
+  border: 1px solid #747d8c;
+  border-radius: 15px;
+  background-color: white;
+  cursor: pointer;
+  transition: box-shadow 0.3s ease;
+  &:hover {
+    border: 1px solid #3742fa;
+    box-shadow: 0 0 0 2px #3742fa;
+  }
+  & > div {
+    width: 100%;
+    border: 1px dashed #70a1ff;
+  }
+`;
+
 const Row = styled.div``;
 const Card = styled.div``;
 
 const CreateTemplate = () => {
+  const [components, setComponents] = useState([]);
+
+  const handleDragStart = (e, id) => {
+    const target = e.target;
+    e.dataTransfer.setData("text/plain", id);
+
+    const dragImage = target.cloneNode(true);
+    dragImage.style.width = "200px";
+    dragImage.style.height = "80px";
+    dragImage.style.border = "2px solid #3742fa";
+    dragImage.style.borderRadius = "15px"; // 원하는 스타일 적용
+    document.body.appendChild(dragImage);
+
+    e.dataTransfer.setDragImage(dragImage, target.offsetWidth / 4, 0);
+
+    setTimeout(() => document.body.removeChild(dragImage), 0);
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const id = e.dataTransfer.getData("text/plain");
+    const newComponent = (
+      <div key={components.length}>
+        <div></div>
+        {id === "two" && <div></div>}
+        {id === "three" && (
+          <>
+            <div></div>
+            <div></div>
+          </>
+        )}
+        {id === "four" && (
+          <>
+            <div></div>
+            <div></div>
+            <div></div>
+          </>
+        )}
+      </div>
+    );
+
+    setComponents([...components, newComponent]);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <Container>
       <Header>
@@ -81,20 +187,51 @@ const CreateTemplate = () => {
         <Profile position={{ top: 60, left: 10 }} />
       </Header>
       <Body>
-        <Screen></Screen>
+        <Screen>
+          <Content onDrop={handleDrop} onDragOver={handleDragOver}>
+            {components.map((Component, index) => (
+              <React.Fragment key={index}>{Component}</React.Fragment>
+            ))}
+          </Content>
+        </Screen>
         <Side>
           <Structures>
             <div>
               <FaCaretDown size={30} />
               <h5>Structures</h5>
             </div>
-            <div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
+            <StructureList>
+              <StructureItem
+                draggable="true"
+                onDragStart={(e) => handleDragStart(e, "one")}
+              >
+                <div></div>
+              </StructureItem>
+              <StructureItem
+                draggable="true"
+                onDragStart={(e) => handleDragStart(e, "two")}
+              >
+                <div></div>
+                <div></div>
+              </StructureItem>
+              <StructureItem
+                draggable="true"
+                onDragStart={(e) => handleDragStart(e, "three")}
+              >
+                <div></div>
+                <div></div>
+                <div></div>
+              </StructureItem>
+              <StructureItem
+                draggable="true"
+                onDragStart={(e) => handleDragStart(e, "four")}
+              >
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+              </StructureItem>
+            </StructureList>
           </Structures>
           <Blocks>
             <div>
