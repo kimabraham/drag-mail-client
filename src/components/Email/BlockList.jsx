39342a109e15c05a08eabb66d7048b1707f0996e
kimabraham <kimkr88@icloud.com>
2024-03-14 21:44:43 +0900

    Feat: divide Block component

---
+++ src/components/Email/BlockList.jsx
@@ -0,0 +1,45 @@
+import { styled } from "styled-components";
+import { PiTextT, PiImage, PiVideoCamera, PiTimerBold } from "react-icons/pi";
+import { FiInstagram } from "react-icons/fi";
+import { RxButton } from "react-icons/rx";
+import { LuAlignVerticalSpaceAround } from "react-icons/lu";
+import { TfiLayoutMenuSeparated } from "react-icons/tfi";
+
+import Card from "./Card";
+
+const Container = styled.div`
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	gap: 20px;
+	background-color: #ecf0f1;
+	padding: 30px;
+`;
+
+const Row = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  gap: 20px;
+`;
+
+const BlockList = () => {
+  return (
+    <Container>
+      <Row>
+        <Card icon={<PiTextT size={30} />} label="Text" />
+        <Card icon={<PiImage size={30} />} label="Image" />
+        <Card icon={<RxButton size={30} />} label="Button" />
+        <Card icon={<LuAlignVerticalSpaceAround size={30} />} label="Space" />
+      </Row>
+      <Row>
+        <Card icon={<PiVideoCamera size={30} />} label="Video" />
+        <Card icon={<FiInstagram size={30} />} label="Social" />
+        <Card icon={<TfiLayoutMenuSeparated size={30} />} label="Menu" />
+        <Card icon={<PiTimerBold size={30} />} label="Timer" />
+      </Row>
+    </Container>
+  );
+};
+
+export default BlockList;
--- src/components/Email/Card.jsx
+++ src/components/Email/Card.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+
 import useDraggable from "../../hooks/useDraggable";
 
 const StyledCard = styled.div`
