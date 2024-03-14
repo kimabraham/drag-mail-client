ef46d8ec14c0283cd0442d146a28a768097fff47
kimabraham <kimkr88@icloud.com>
2024-03-14 21:48:25 +0900

    Feat: loading component with spinner

--- src/components/shared/Loading.jsx
+++ src/components/shared/Loading.jsx
@@ -1,79 +1,43 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & > h5 {
+    margin-left: 10px;
+    color: #70a1ff;
+    letter-spacing: .5px;
+  }
+`;
+
+const Spinner = styled.div`
+  border: 4px solid #bcd3ff;
+  border-top: 4px #1e90ff solid;
+  border-radius: 50%;
+  height: 40px;
+  width: 40px;
+  animation: spin 2s linear infinite;
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
 const Loading = () => {
   return (
-    <svg className="h-6 w-6 animate-spin stroke-gray" viewBox="0 0 256 256">
-      <line
-        x1="128"
-        y1="32"
-        x2="128"
-        y2="64"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="24"
-      ></line>
-      <line
-        x1="195.9"
-        y1="60.1"
-        x2="173.3"
-        y2="82.7"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="24"
-      ></line>
-      <line
-        x1="224"
-        y1="128"
-        x2="192"
-        y2="128"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="24"
-      ></line>
-      <line
-        x1="195.9"
-        y1="195.9"
-        x2="173.3"
-        y2="173.3"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="24"
-      ></line>
-      <line
-        x1="128"
-        y1="224"
-        x2="128"
-        y2="192"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="24"
-      ></line>
-      <line
-        x1="60.1"
-        y1="195.9"
-        x2="82.7"
-        y2="173.3"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="24"
-      ></line>
-      <line
-        x1="32"
-        y1="128"
-        x2="64"
-        y2="128"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="24"
-      ></line>
-      <line
-        x1="60.1"
-        y1="60.1"
-        x2="82.7"
-        y2="82.7"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="24"
-      ></line>
-    </svg>
+    <Container>
+      <Spinner />
+      <h5>Loading...</h5>
+    </Container>
   );
 };
 
